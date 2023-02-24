ec902b59083a00e3d87ea000877c7dad1b08cfe3
Bhavnesh Sharma <bhanusharma252001@gmail.com>
2023-02-24 21:33:29 +0530

    add scripts

--- index.js
+++ index.js
@@ -1,77 +1,73 @@
 const express = require('express');
-const app = express();
 const { v4 : uuidv4 } = require('uuid')
 var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
 
-const dotenv = require('dotenv').config();
-const db = require("./db/db")
+const app = express();
+
+app.use(bodyParser.json());
+app.use( bodyParser.urlencoded({ extended: true }));
  
 let authors = [ ];
-let books = [];
+let books = [ ];
 
-app.get("/",(req,res)=>
-{
-    res.json({message:"Welcome to Server"})
+app.get("/",( req, res )=> {
+    res.status(200).json({ message:"Welcome to otobit server" })
 })
 
 
 // author module
-app.post("/author",( req, res)=>
-{
+app.post("/author",( req, res ) => {
   if(authors.length!=0) {
     const isNameExist = authors.find(author=>author.name === req.body.name)
     if(isNameExist) return res.status(400).json({ message:'failure',error:'author already exist'})
   }
+
   const createdAuthor = {...req.body,id:uuidv4()}
   authors.push(createdAuthor)
+
   return res.status(200).json({ 
     message:'success',
     user:createdAuthor })
 })
 
-app.get("/author", ( req, res)=>
-{
+app.get("/author", ( req, res )=> {
   return res.status(200).json({ message:'success', authors})
 })
 
-app.patch("/author/:id", ( req, res)=>{
+app.patch("/author/:id", ( req, res)=> {
  
   const foundAuthor = authors.find((author)=>author.id === req.params.id);
   if(!foundAuthor)  return res.status(400).json({ message:'failure',error:'author id does not exist'})
 
   const isNameExist = authors.find(author=>author.name === req.body.name)
   if(isNameExist) return res.status(400).json({ message:'failure',error:'author name already exist'})
- let updatedAuthor;
+ 
+  let updatedAuthor;
+
   for (const author of authors) {
     if(author.id === req.params.id){
       author.name = req.body.name;
       updatedAuthor = author;
     }
   }
+
   return res.status(200).json({ 
     message:'success',
     user:updatedAuthor })
 })
 
-app.delete("/author/:id", (req, res)=>
-{
+app.delete("/author/:id", (req, res)=> {
   const foundAuthor = authors.find((author)=>author.id === req.params.id);
   if(!foundAuthor)  return res.status(400).json({ message:'failure',error:'author id does not exist'})
 
   authors = authors.filter((author)=>author.id !== req.params.id);
+
   return res.status(200).json({ 
     message:'success',
     deletedAuthor:foundAuthor })
 })
 
-app.get("/author/getAllbooksOfAuthor/:id", ( req, res)=>
-{
+app.get("/author/getAllbooksOfAuthor/:id", ( req, res)=> {
   const foundAuthor = authors.find((author)=>author.id === req.params.id);
   if(!foundAuthor)  return res.status(400).json({ message:'failure',error:'author id does not exist'});
 
@@ -85,26 +81,26 @@ app.get("/author/getAllbooksOfAuthor/:id", ( req, res)=>
 })
 
 
-// Book Module 
-app.post("/book",(req,res)=>
-{
-  if(books.length!=0){
+// book module 
+app.post("/book",( req, res ) => {
+  if( books.length != 0 ) {
     const isISBNExist = books.find((book)=>book.ISBN === req.body.ISBN);
     if(isISBNExist) return res.status(400).json( { message: 'failure', error: ' ISBN already exists'})
   }
-  const createdBook = {...req.body,id:uuidv4()}
+
+  const createdBook = { ...req.body, id:uuidv4() }
   books.push(createdBook)
+
   return res.status(200).json({ 
     message:'success',
     user:createdBook })
 })
 
-app.get("/book", ( req, res)=>
-{
+app.get("/book", ( req, res ) => {
   return res.status(200).json({ message:'success', books})
 })
 
-app.patch("/book/:id", ( req, res)=>{
+app.patch("/book/:id", ( req, res ) => {
  
   const foundBook = books.find((book)=>book.id === req.params.id);
   if(!foundBook)  return res.status(400).json({ message:'failure',error:'book id does not exist'})
@@ -127,18 +123,18 @@ app.patch("/book/:id", ( req, res)=>{
     updatedBook })
 })
 
-app.delete("/book/:id", (req, res)=>
-{
+app.delete("/book/:id", (req, res )=> {
   const foundBook = books.find((book)=>book.id === req.params.id);
   if(!foundBook)  return res.status(400).json({ message:'failure',error:'book id does not exist'})
 
   books = books.filter((book)=>book.id !== req.params.id);
+
   return res.status(200).json({ 
     message:'success',
     deletedBook:foundBook })
 })
 
-app.get("/getBookWithAuthor/:id", ( req, res)=>{
+app.get("/getBookWithAuthor/:id", ( req, res )=> {
   const foundBook = books.find((book)=>book.id === req.params.id);
   if(!foundBook)  return res.status(400).json({ message:'failure',error:'book id does not exist'});
 
@@ -152,9 +148,7 @@ app.get("/getBookWithAuthor/:id", ( req, res)=>{
 
 const PORT = process.env.port || 3000 ;
 
-app.listen(PORT,(err)=>
-{
+app.listen(PORT,(err)=> {
 if(err) throw err;
-else console.log(`Server is running on port ${process.env.port}`);
-
+else console.log(`Server is running on port ${PORT}`);
 })
